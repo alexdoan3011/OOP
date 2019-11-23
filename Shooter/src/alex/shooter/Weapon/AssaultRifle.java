09f2da573eb8bf6f5611527257a880155ce26613
alexdoan3011 <alexdoan.3011@gmail.com>
2019-11-23 13:16:57 -0500

    IU-2019.2.4 <Alex@DESKTOP-QF78018 Overwrite remote https://github.com/alexdoan3011/OOP to local

--- Shooter/src/alex/shooter/Weapon/AssaultRifle.java
+++
@@ -1,8 +0,0 @@
-package alex.shooter.Weapon;
-
-public class AssaultRifle {
-    public static final double ASSAULT_RIFLE_SPREAD = 0.02;
-    public static final double ASSAULT_RIFLE_PROJ_SPEED = 10;
-    public static final double SHOT_PER_MIN_ASSAULT_RIFLE = 1000;
-    public static final int ASSAULT_RIFLE_PROJECTILE_DAMAGE = 10;
-}
--- Test/src/A.java
+++
@@ -1,5 +0,0 @@
-public class A {
-	public void doit() {
-		System.out.print("a");
-	}
-}
